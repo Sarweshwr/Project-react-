d29c88b8a536bc439bdae0523bff96669dd076ad
Sarweshwar Prasad <sarweshwarprasad007@gmail.com>
2024-08-24 13:54:36 +0530

    Add files via upload

---
+++ account.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Sidebar from "./sidebar";
+export default function Account(){
+  return(
+    <>
+    <Header/>
+    <Sidebar/>
+    account
+    <Footer/>
+    </>
+    )
+}
